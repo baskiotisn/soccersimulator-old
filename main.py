932983b21aa6fc60ea1c5a7f380444964ea1c596
baskiotisn <nicolas.baskiotis@gmail.com>
2015-02-22 20:58:27 +0100

    main.py vrai listze

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ import pickle
 
 TARGET_PATH="/tmp/tournoi"
 
-#GIT_LIST_2015=[("Benlog","Soccer-AI"),("SebXIII","Projet"),("andrenasturas","2I013"),("AlexandreCasanova","projet"),\
-#            ("mariene","projet_foot"),("Leynad","ProjetFoot"),("ArezkiSky","Projet-soccer-2I013"),("Maumiz","ProjetSoccer"),\
-#            ("kabylemak","soccerproject"),("timotheb","projet"),("orangemango","orange-soccer-team")]
+GIT_LIST_2015=[("Benlog","Soccer-AI"),("SebXIII","Projet"),("andrenasturas","2I013"),("AlexandreCasanova","projet"),\
+            ("mariene","projet_foot"),("Leynad","ProjetFoot"),("ArezkiSky","Projet-soccer-2I013"),("Maumiz","ProjetSoccer"),\
+            ("kabylemak","soccerproject"),("timotheb","projet"),("orangemango","orange-soccer-team")]
 
-GIT_LIST_2015=[("Benlog","Soccer-AI"),("SebXIII","Projet")]
 
 def load_from_github(login,project,git_import=True):
     # clone le depot github correspondant
