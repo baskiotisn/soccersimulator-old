12e242a69328659ae3798e7730f6516801bf060b
baskiotisn <nicolas.baskiotis@gmail.com>
2016-01-17 20:19:20 +0100

    version 2015

---
+++ soccersimulator/__init__.py
@@ -0,0 +1,5 @@
+from utils import Vector2D, MobileMixin
+from mdpsoccer import SoccerAction, Configuration, SoccerTeam, SoccerState, Player
+from mdpsoccer import SoccerMatch, SoccerEvents, AbstractStrategy, SoccerTournament, Score
+from interfaces import MatchWindow, show
+import settings
