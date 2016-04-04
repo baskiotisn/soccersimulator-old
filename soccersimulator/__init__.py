e8b1ea36272151bec60848e555acc21212193efb
baskiotisn <nicolas.baskiotis@gmail.com>
2016-04-04 11:19:52 +0200

    Update __init__.py

--- soccersimulator/__init__.py
+++ soccersimulator/__init__.py
@@ -1,6 +1,6 @@
 from utils import Vector2D, MobileMixin
 from mdpsoccer import SoccerAction, Configuration, SoccerTeam, SoccerState, Player, SoccerPlayer, AbstractStrategy
-from mdpsoccer import SoccerMatch, SoccerEvents, BaseStrategy, SoccerTournament, Score, KeyboardStrategy
+from mdpsoccer import Ball,SoccerMatch, SoccerEvents, BaseStrategy, SoccerTournament, Score, KeyboardStrategy
 from interfaces import MatchWindow, show
 import settings
 from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor, export_graphviz
